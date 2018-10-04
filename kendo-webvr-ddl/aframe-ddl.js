f1770426d2ae9b048e8a932dca0f29ecb6f6ecf7
Yanko Dzhemerenov <iankodj@gmail.com>
2018-10-04 16:44:47 +0300

    ddl popup

--- kendo-webvr-ddl/aframe-ddl.js
+++ kendo-webvr-ddl/aframe-ddl.js
@@ -1,6 +1,6 @@
 AFRAME.registerComponent('dropdownlist', {
     schema: {
-        data: { type: 'asset' },
+        dataSource: {},
         width: { type: 'number' }
     }, 
     init: function () {
@@ -11,28 +11,89 @@ AFRAME.registerComponent('dropdownlist', {
         this.el.appendChild(this.wrapper);
 
         this.btn = document.createElement('a-entity');
-        this.btn.setAttribute("id", "k-ddl-toggle-button")
-        this.btn.setAttribute('position', '0 0 0.02');
+        this.btn.setAttribute("class", "k-ddl-toggle-button")
+        this.btn.setAttribute('position', '1.75 0 0.02');
         this.btn.setAttribute('geometry', {primitive: "plane", height: 0.5, width: 0.5});
         this.btn.setAttribute('material', {color: "#727272"});
+        this.btn.setAttribute('text', {value: "<", color: "red",  wrapCount: "1"});
         this.el.appendChild(this.btn);
 
-        document.querySelector("#k-ddl-toggle-button").addEventListener("click", function () {
-            alert(1)
+        this.popup = document.createElement('a-entity');
+        this.popup.setAttribute("class", "k-ddl-popup");
+        this.popup.setAttribute('position', '0 -0.51 0.01');
+        this.popup.setAttribute('geometry', {primitive: "plane", width: this.data.width});
+        this.popup.setAttribute('material', {transperant: true, opacity: 0});
+        this.el.appendChild(this.popup);
+
+        // var item = document.createElement('a-entity');
+        // item.setAttribute("position", "0 0 0.03");
+        // item.setAttribute('geometry', {primitive: "plane", height: 0.5, width: this.data.width});
+        // item.setAttribute("text", {value: "hello", color: "red", wrapCount: 20});
+        // item.setAttribute('material', {color: "#727272"});
+        // this.popup.appendChild(item);
+
+        // item = document.createElement('a-entity');
+        // item.setAttribute("position", "0 -0.5 0.03");
+        // item.setAttribute('geometry', {primitive: "plane", height: 0.5, width: this.data.width});
+        // item.setAttribute("text", {value: "hello1", color: "red", wrapCount: 20});
+        // item.setAttribute('material', {color: "#727272"});
+        // this.popup.appendChild(item);
+
+        var that = this;
+        that.visible = false;
+        this.el.addEventListener("click", function (ev) { 
+             that.toggle() 
         });
 
-        // this.el.addEventListener("click", function () {
-        //     console.log(1);
-        // });
+        this.el.addEventListener("mouseenter", function (ev) { 
+            if(ev.srcElement.className && ev.srcElement.className === "k-item"){
+                var origPos = ev.srcElement.getAttribute("position");
+                origPos.z = 0.1;
+                ev.srcElement.setAttribute("position", origPos);
+            }
+        });
+
+        this.el.addEventListener("mouseleave", function (ev) { 
+            if(ev.srcElement.className && ev.srcElement.className === "k-item"){
+                var origPos = ev.srcElement.getAttribute("position");
+                origPos.z = 0.03;
+                ev.srcElement.setAttribute("position", origPos);
+            }
+        });
     },
     update: function () {
-
     },
-    loadData: function () {
+    loadData: function (data) {
+        this.data.dataSource = data;
+        var height = 0;
+        var item;
 
+        for(var i = 0; i < data.length; i++){
+            item = document.createElement('a-entity');
+            item.setAttribute("class", "k-item");
+            item.setAttribute("position", "0 -" + height + " 0.03");
+            item.setAttribute('geometry', {primitive: "plane", height: 0.5, width: this.data.width});
+            item.setAttribute("text", {value: data[i], color: "red", wrapCount: 20});
+            item.setAttribute('material', {color: "#727272"});
+            // item.setAttribute("light", {type: "directional", castShadow: true});
+            // light="type:directional; castShadow:true;"
+            this.popup.appendChild(item);
+            console.log("0 -" + height + "0.03")
+            console.log(data[i])
+            height += 0.5;
+        }
     },
     toggle: function () {
-        debugger
+        // debugger
+        
+        this.btn.setAttribute('text', {value: this.visible ? ">" : "<"})
+        console.log(1);
+        console.log(this.visible)
+
+            this.popup.setAttribute("visible", this.visible ? "false" : "true");
+
+        this.visible = !this.visible;
+
 
     }
 });
@@ -41,7 +102,7 @@ AFRAME.registerPrimitive('kendo-webvr-dropdownlist', {
         dropdownlist: {}
     },
     mappings: {
-        data: 'dropdownlist.data',
+        dataSource: 'dropdownlist.dataSource',
         width: 'dropdownlist.width'
     }
 });
