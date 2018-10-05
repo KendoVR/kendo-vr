49c2b74be14f328bbd23c21fb9d24fdf3f1606d5
Yanko Dzhemerenov <iankodj@gmail.com>
2018-10-05 10:58:09 +0300

    ddl selection state

--- kendo-webvr-ddl/aframe-ddl.js
+++ kendo-webvr-ddl/aframe-ddl.js
@@ -1,81 +1,105 @@
 AFRAME.registerComponent('dropdownlist', {
     schema: {
         dataSource: {},
+        opacity: {type: 'number', default: 0.5 },
         width: { type: 'number' },
+        rowHeight: { type: 'number', default: 0.56 },
+        textColor: { type: 'string', default: "#3f5971" },
         value: { type: 'string' }
     }, 
     init: function () {
         this.wrapper = document.createElement('a-entity');
         this.wrapper.setAttribute('position', '0 0 0.01');
-        this.wrapper.setAttribute('geometry', {primitive: "plane", height: 0.5, width: this.data.width});
-        this.wrapper.setAttribute('material', {color: "#bababa"});
-        this.wrapper.setAttribute('text', {color: "red", value: "", wrapCount: 20});
-        this.wrapper.setAttribute('material', {transperant: true, opacity: 0.5});
+        this.wrapper.setAttribute('geometry', {primitive: "plane", height: this.data.rowHeight, width: this.data.width});
+        this.wrapper.setAttribute('material', {color: "white"});
+        this.wrapper.setAttribute('text', {color: this.data.textColor, value: "", width: this.data.width * 1.5, xOffset: this.data.width / 2 - 0.8});
+        this.wrapper.setAttribute('material', {transperant: true, opacity: this.data.opacity});
         this.el.appendChild(this.wrapper);
 
         this.btn = document.createElement('a-entity');
         this.btn.setAttribute("class", "k-ddl-toggle-button")
         this.btn.setAttribute('position', this.data.width / 2 - 0.25 + ' 0 0.02');
-        this.btn.setAttribute('geometry', {primitive: "plane", height: 0.5, width: 0.5});
-        this.btn.setAttribute('material', {color: "#727272"});
-        this.btn.setAttribute('text', {value: "<", color: "red",  wrapCount: "1"});
-        this.btn.setAttribute('material', {transperant: true, opacity: 0.5});
+        this.btn.setAttribute('geometry', {primitive: "plane", height: this.data.rowHeight, width: 0.5});
+        this.btn.setAttribute('material', {color: "white"});
+        this.btn.setAttribute('text', {value: "<", color: "black",  wrapCount: "1"});
+        this.btn.setAttribute('material', {transperant: true, opacity: 0});
         this.el.appendChild(this.btn);
 
         this.popup = document.createElement('a-entity');
         this.popup.setAttribute("class", "k-ddl-popup");
-        this.popup.setAttribute('position', '0 -0.51 0.01');
+        this.popup.setAttribute('position', '0 -' + (this.data.rowHeight + 0.04) + ' 0.01');
         this.popup.setAttribute('geometry', {primitive: "plane", width: this.data.width});
         this.popup.setAttribute('material', {transperant: true, opacity: 0});
-        this.popup.setAttribute('visible', false);
+        this.popup.setAttribute('visible', true);
         this.el.appendChild(this.popup);
-
-        // var item = document.createElement('a-entity');
-        // item.setAttribute("position", "0 0 0.03");
-        // item.setAttribute('geometry', {primitive: "plane", height: 0.5, width: this.data.width});
-        // item.setAttribute("text", {value: "hello", color: "red", wrapCount: 20});
-        // item.setAttribute('material', {color: "#727272"});
-        // this.popup.appendChild(item);
-
-        // item = document.createElement('a-entity');
-        // item.setAttribute("position", "0 -0.5 0.03");
-        // item.setAttribute('geometry', {primitive: "plane", height: 0.5, width: this.data.width});
-        // item.setAttribute("text", {value: "hello1", color: "red", wrapCount: 20});
-        // item.setAttribute('material', {color: "#727272"});
-        // this.popup.appendChild(item);
 
         var that = this;
         that.visible = false;
         this.el.addEventListener("click", function (ev) { 
              that.toggle()
 
-             if(ev.srcElement.className && ev.srcElement.className === "k-item") {
+             if(ev.srcElement.className && /k-item/.test(ev.srcElement.className)) {
                 var newValue = ev.srcElement.components.text.data.value;
                 that.el.dispatchEvent( new CustomEvent("change", {detail: {oldValue: that.data.value, newValue: newValue}}) );
                 that.data.value = newValue;
+                that.selectedItem = ev.srcElement;
                 that.update();
              }
         });
 
         this.el.addEventListener("mouseenter", function (ev) { 
-            if(ev.srcElement.className && ev.srcElement.className === "k-item"){
-                var origPos = ev.srcElement.getAttribute("position");
-                origPos.z = 0.1;
-                ev.srcElement.setAttribute("position", origPos);
+            if(ev.srcElement.className && /k-item/.test(ev.srcElement.className)){
+                // var origPos = ev.srcElement.getAttribute("position");
+                var origMaterial = ev.srcElement.getAttribute("material");
+                // var origText = ev.srcElement.getAttribute("text");
+                origMaterial.opacity = 1;
+                // origPos.z = 0.1;
+                // origText.color = "white";
+                // ev.srcElement.setAttribute("position", origPos);
+                ev.srcElement.setAttribute('material', origMaterial);
+                // ev.srcElement.setAttribute('text', origText);
             }
         });
 
         this.el.addEventListener("mouseleave", function (ev) { 
-            if(ev.srcElement.className && ev.srcElement.className === "k-item"){
-                var origPos = ev.srcElement.getAttribute("position");
-                origPos.z = 0.03;
-                ev.srcElement.setAttribute("position", origPos);
+            if(ev.srcElement.className &&  /k-item/.test(ev.srcElement.className)){
+                
+                // var origPos = ev.srcElement.getAttribute("position");
+                var origMaterial = ev.srcElement.getAttribute("material");
+                // var origText = ev.srcElement.getAttribute("text");
+                // origPos.z = 0.03;
+                origMaterial.opacity = that.data.opacity;
+                // origText.color = "black"
+                // ev.srcElement.setAttribute("position", origPos);
+                ev.srcElement.setAttribute('material', origMaterial);
             }
         });
     },
     update: function () {
         if(this.data.value && this.data.value !== "") {
             this.wrapper.setAttribute("text", {value: this.data.value});
+
+            var oldSelected = document.querySelector(".k-selected");
+            if(oldSelected) {
+                var origText = oldSelected.getAttribute("text");
+                var origMaterial = oldSelected.getAttribute("material");
+                origMaterial.color = "white";
+                origText.color = this.data.textColor;
+                oldSelected.setAttribute("material", origMaterial);
+                oldSelected.setAttribute("text", origText);
+                oldSelected.setAttribute("class", "k-item");
+            }
+            
+
+            if(this.selectedItem){
+                var origText = this.selectedItem.getAttribute("text");
+                var origMaterial = this.selectedItem.getAttribute("material");
+                origMaterial.color = "#199cad";
+                origText.color = "white";
+                this.selectedItem.setAttribute("material", origMaterial);
+                this.selectedItem.setAttribute("text", origText);
+                this.selectedItem.setAttribute("class", "k-item k-selected");
+            }
         }
     },
     loadData: function (data) {
@@ -86,13 +110,21 @@ AFRAME.registerComponent('dropdownlist', {
         for(var i = 0; i < data.length; i++){
             item = document.createElement('a-entity');
             item.setAttribute("class", "k-item");
-            item.setAttribute("position", "0 -" + height + " 0.03");
-            item.setAttribute('geometry', {primitive: "plane", height: 0.5, width: this.data.width});
-            item.setAttribute("text", {value: data[i], color: "red", wrapCount: 20});
-            item.setAttribute('material', {color: "#727272"});
-            item.setAttribute('material', {transperant: true, opacity: 0.5});
+            item.setAttribute("position", "0 -" + (height) + " 0.03");
+            item.setAttribute('geometry', {primitive: "plane", height: this.data.rowHeight, width: this.data.width});
+            
+
+            if(data[i] === this.data.value){
+                item.setAttribute('material', {color: "#199cad", transperant: true, opacity: this.data.opacity});
+                item.setAttribute('text', {value: data[i], color: "white", width: this.data.width * 1.5, xOffset: this.data.width / 2 - 0.8});
+                item.setAttribute("class", "k-item k-selected")
+            }else{
+                item.setAttribute('material', {color: "white", transperant: true, opacity: this.data.opacity});
+                item.setAttribute("text", {value: data[i], color: this.data.textColor, width: this.data.width * 1.5, xOffset: this.data.width / 2 - 0.8});
+            }
+
             this.popup.appendChild(item);
-            height += 0.5;
+            height += this.data.rowHeight + 0.005;
         }
     },
     toggle: function () {
@@ -108,6 +140,8 @@ AFRAME.registerPrimitive('kendo-webvr-dropdownlist', {
     mappings: {
         dataSource: 'dropdownlist.dataSource',
         width: 'dropdownlist.width',
-        value: 'dropdownlist.value'
+        value: 'dropdownlist.value',
+        opacity: 'dropdownlist.opacity',
+        "row-height": 'dropdownlist.rowHeight'
     }
 });
